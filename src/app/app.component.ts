a1bdafc3e2a82793b7809c551554ae9bf777a428
Anderson Oliveira <anderson.sfoliveira@gmail.com>
2021-09-27 20:44:31 -0300

    second commit

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,4 +1,7 @@
-import { Component } from '@angular/core';
+import { Component, VERSION } from '@angular/core';
+import { FormGroup, FormControl, Validators } from '@angular/forms';
+
+import { ReCaptchaV3Service } from 'ng-recaptcha';
 
 @Component({
   selector: 'app-root',
@@ -6,4 +9,41 @@ import { Component } from '@angular/core';
   styleUrls: ['./app.component.css']
 })
 export class AppComponent {
+  public version = VERSION.full;
+  public recaptchaMode = 'v3';
+  public reactiveForm: FormGroup = new FormGroup({
+    recaptchaReactive: new FormControl(null, Validators.required)
+  });
+  public log: string[] = [];
+
+  constructor(private recaptchaV3Service: ReCaptchaV3Service) {}
+
+  public executeRecaptchaV3() {
+    this.log.push(`Recaptcha v3 execution requested...`);
+    this.recaptchaV3Service.execute('myAction').subscribe(
+      token => {
+        this.addTokenLog('Recaptcha v3 token', token);
+      },
+      error => {
+        this.log.push(`Recaptcha v3 error: see console`);
+        console.log(`Recaptcha v3 error:`, error);
+      }
+    );
+  }
+
+  public addTokenLog(message: string, token: string | null) {
+    this.log.push(`${message}: ${this.formatToken(token)}`);
+  }
+
+  public formatToken(token: string | null) {
+    return token !== null
+      ? `${token.substr(0, 7)}...${token.substr(-7)}`
+      : 'null';
+  }
+
+  public printLog() {
+    return this.log
+      .map((logEntry, index) => `${index + 1}. ${logEntry}`)
+      .join('\n');
+  }
 }
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -2,15 +2,36 @@ import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
 
 import { AppComponent } from './app.component';
+import { RecaptchaFormsModule, RecaptchaModule, RecaptchaSettings, RecaptchaV3Module, RECAPTCHA_SETTINGS, RECAPTCHA_V3_SITE_KEY } from 'ng-recaptcha';
+import { FormsModule, ReactiveFormsModule } from '@angular/forms';
+
+const RECAPTCHA_V3_STACKBLITZ_KEY = '6LeHBK0bAAAAAOQVTvBOWhfb08cQfUpFoSE3FsmP';
+const RECAPTCHA_V2_DUMMY_KEY = '6LeIxAcTAAAAAJcZVRqyHh71UMIEGNQ_MXjiZKhI';
 
 @NgModule({
   declarations: [
     AppComponent
   ],
   imports: [
-    BrowserModule
+    BrowserModule,
+    FormsModule,
+    ReactiveFormsModule,
+    RecaptchaModule,
+    RecaptchaFormsModule,
+    RecaptchaV3Module
   ],
-  providers: [],
+  providers: [
+    {
+      provide: RECAPTCHA_V3_SITE_KEY,
+      useValue: RECAPTCHA_V3_STACKBLITZ_KEY
+    },
+    {
+      provide: RECAPTCHA_SETTINGS,
+      useValue: {
+        siteKey: RECAPTCHA_V2_DUMMY_KEY
+      } as RecaptchaSettings
+    }
+  ],
   bootstrap: [AppComponent]
 })
 export class AppModule { }
